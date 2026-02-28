f6df7edc95b56d2d36a26e57b9c164a00b10710c
23f2002146-nivesh <23f2002146@ds.study.iitm.ac.in>
2026-02-28 22:09:24 +0530

    Create scrape.js

---
+++ scrape.js
@@ -0,0 +1,26 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+
+  const seeds = [44,45,46,47,48,49,50,51,52,53];
+
+  let totalSum = 0;
+
+  for (const seed of seeds) {
+    const url = `https://sanand0.github.io/tdsdata/playwright_seed_${seed}.html`;
+    await page.goto(url);
+
+    const numbers = await page.$$eval('table td', cells =>
+      cells.map(cell => parseFloat(cell.innerText)).filter(n => !isNaN(n))
+    );
+
+    const pageSum = numbers.reduce((a, b) => a + b, 0);
+    totalSum += pageSum;
+  }
+
+  console.log("FINAL_TOTAL_SUM=" + totalSum);
+
+  await browser.close();
+})();
